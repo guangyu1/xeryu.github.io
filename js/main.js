e899655d71df7d27290bcb3bbc76ff8010adc8b3
guangyu1 <112939719+guangyu1@users.noreply.github.com>
2022-09-06 18:14:24 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function playMusic() {
+        var audioEle = document.getElementById("audio");
+      if (audioEle.paused){
+        audioEle.play();
+      }else {
+        audioEle.pause();
+      }
+    }
+    
+    $("#music").click(function(){
+      play();
+    });
+
+    var player = document.getElementById("audio");
+    play();
+    function play(){
+    swal("欢迎浏览啭脚主页\n请问是否开启音乐\n一边浏览一边听呢？", {
+            buttons: {
+                cancel: "开启",
+                allow: "关闭"
+            }
+        }).then(function(value) {
+            if (value == "allow") {
+                player.pause()
+            } else {
+                player.play();
+            }
+        });
+    }
